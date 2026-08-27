51ab05c807c9bcf648d5014aa28d73b7a2bed3f9
Brad Brown Jr <bradbrownjr@gmail.com>
2026-08-27 12:44:49 -0400

    Updated comments for clarity

--- Linux/Troubleshooting/USB Serial Device Permissions.sh
+++ Linux/Troubleshooting/USB Serial Device Permissions.sh
@@ -1,3 +1,4 @@
 # The ‘user’ profile in some distributions like Ubuntu does not have direct access to the serial interfaces, commonly or formerly used for modem connections. 
+# Logs may indicate a PERMISSION DENIED error when trying to access a USB serial device.
 # Run this, then log out and back in, or just reboot
 gpasswd –add ${USER} dialout
